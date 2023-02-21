51ff04f466b17a15ec118556391a5853b988fdee
Anzzhhela98 <anzzhhela98@gmail.com>
2023-02-21 20:36:04 +0200

    The server can accept more than one user using async Task methods

--- HttpRequester/Program.cs
+++ HttpRequester/Program.cs
@@ -19,51 +19,57 @@ namespace HttpRequester
                 //set using to close conntection
                 //acceptClient
                 var client = listener.AcceptTcpClient();
-                using (var stream = client.GetStream())
-                {
-                    //readFromStream
-                    byte[] buffer = new byte[10000];
-
-                    var length = stream.Read(buffer, 0, buffer.Length);
-
-                    Encoding.UTF8.GetString(buffer).ToString();
-                    var requestString = Encoding.UTF8.GetString(buffer, 0, buffer.Length).ToString();
-
-
-                    //response
-                    string html = $"<h1>Hello From Anzhela {DateTime.UtcNow}";
-
-                    string newLine = "\r\n";
-                    string response =
-                        "HTTP/1.1 200 OK" + newLine +
-                        "Server: Appache/1.3.29 Win(64)" + newLine +
-                        "Accept-Ranges: bytes" + newLine +
-                        "Content-Type: text/html" + newLine +
-                        "Content-Length: " + html.Length + newLine +
-                        newLine +
-                        html + newLine;
-
-                    string responseRedirect =
-                        "HTTP/1.1 307 Redirect" + newLine +
-                        "Server: Appache/1.3.29 Win(64)" + newLine +
-                        "Location: https://www.google.com" + newLine +
-                        "Accept-Ranges: bytes" + newLine +
-                        "Content-Type: text/html" + newLine +
-                        "Content-Length: " + html.Length + newLine +
-                        newLine +
-                        html + newLine;
-
-                    var responseBytes = Encoding.UTF8.GetBytes(response);
-
-
-                    stream.Write(responseBytes);
-                }
+                await ProcessClientAsync(client);
             }
 
             //Console.OutputEncoding = Encoding.UTF8;
             //await ReadHeaders();
             //await ReadHtml();
         }
+
+        public static async Task ProcessClientAsync(TcpClient client)
+        {
+            using (var stream = client.GetStream())
+            {
+                //readFromStream
+                byte[] buffer = new byte[10000];
+
+                var length = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                Encoding.UTF8.GetString(buffer).ToString();
+                var requestString = Encoding.UTF8.GetString(buffer, 0, buffer.Length).ToString();
+
+
+                //response
+                string html = $"<h1>Hello From Anzhela {DateTime.UtcNow}";
+
+                string newLine = "\r\n";
+                string response =
+                    "HTTP/1.1 200 OK" + newLine +
+                    "Server: Appache/1.3.29 Win(64)" + newLine +
+                    "Accept-Ranges: bytes" + newLine +
+                    "Content-Type: text/html" + newLine +
+                    "Content-Length: " + html.Length + newLine +
+                    newLine +
+                    html + newLine;
+
+                string responseRedirect =
+                    "HTTP/1.1 307 Redirect" + newLine +
+                    "Server: Appache/1.3.29 Win(64)" + newLine +
+                    "Location: https://www.google.com" + newLine +
+                    "Accept-Ranges: bytes" + newLine +
+                    "Content-Type: text/html" + newLine +
+                    "Content-Length: " + html.Length + newLine +
+                    newLine +
+                    html + newLine;
+
+                var responseBytes = Encoding.UTF8.GetBytes(response);
+
+
+                await stream.WriteAsync(responseBytes);
+            }
+        }
+
         public static async Task ReadHtml()
         {
             string url = "https://softuni.bg/";
